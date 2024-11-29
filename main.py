15218f514c2c1922c169f9a7cb961180192ecd53
Wessias <viggowessner@gmail.com>
2024-11-29 23:15:53 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+#Såg att i lecture notes 8 finns ett tillämpnings exempel: Collective dynamics of pedestrians in a corridor
+#som verkar matcha vårt arbete ganska väl, se ->  https://www.wellesu.com/10.1103/PhysRevE.102.022307
+#Specifikt känns det som att model (c) (Social Force Model + Vicsek Model) är exakt vad vi vill ha.
+# SFM använder sig av en "desire force" + "social force" (agents vill inte vara för nära varandra) + "wall force" (inte gå in i väg) 
+# och Vicsek ger oss att agents vill matcha individer i närheten //Viggo
