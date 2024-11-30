c032ffd411a0a46c7b59fb1b9e7dca55ac68fbc3
Wessias <viggowessner@gmail.com>
2024-11-30 01:30:39 +0100

    Update main.py
    blev uttråkad och lekte med markdown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-#Såg att i lecture notes 8 finns ett tillämpnings exempel: Collective dynamics of pedestrians in a corridor
+# %% [markdown]
+#Såg att i lecture notes 8 finns ett tillämpnings exempel: *Collective dynamics of pedestrians in a corridor*
 #som verkar matcha vårt arbete ganska väl, se ->  https://www.wellesu.com/10.1103/PhysRevE.102.022307
+#
 #Specifikt känns det som att model (c) (Social Force Model + Vicsek Model) är exakt vad vi vill ha.
 # SFM använder sig av en "desire force" + "social force" (agents vill inte vara för nära varandra) + "wall force" (inte gå in i väg) 
 # och Vicsek ger oss att agents vill matcha individer i närheten //Viggo
@@ -96,4 +98,7 @@ def wall_force():
 def granular_wall_force():
     return
 
+# %% [markdown]
 # (SFM model) velocity(t+dt) = velocity(t) + (1/m)(desire_force + social_force + granular_force + wall_force + granular_wall_force) 
+# $$ v^{\textbf{SFM}}(t + \Delta t) = \frac{1}{m}( \bm{F_{D} + F_{S} + F_{G} + F_{W} + F_{GW}})$$
+# %%
